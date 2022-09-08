a03e0d55a0185ef679b853437c0ccb200d9ead1f
Arzamasov <Arzamasov.alexander@gmail.com>
2022-09-08 00:32:50 -0700

    small fix to the abstract

--- code/qc_read_mapping.sh
+++ code/qc_read_mapping.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+source ~/.bash_profile
 echo $BASH_VERSION
 set -ex
 
-# required software: fastqc (v0.11.9), cutadapt (v3.4), bowtie2 (v2.4.4), kallisto (v0.46.2), multiqc (v1.11), and parallel (v20210222)
+# required software: fastqc (v0.11.9), cutadapt (v3.4), bowtie2 (v2.4.4), kallisto (v0.46.2)
+# multiqc (v1.11), and parallel (v20210222)
 # sample names should be in data/rnaseq/runids.txt
-
 # activate conda enviroment with required software
 eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)" # initializes conda in sub-shell
 conda activate transcriptomics
@@ -19,7 +19,8 @@ mkdir data/rnaseq/sam # sam files produced during bowtie2 alignment; will be del
 mkdir data/rnaseq/kallisto # kallisto mapping results
 
 # run fastqc on raw reads
-cat data/rnaseq/runids.txt | parallel "fastqc data/rnaseq/fastq/{} --outdir data/rnaseq/qc1"
+cat data/rnaseq/runids.txt | parallel "fastqc data/rnaseq/fastq/{}.fastq.gz \
+--outdir data/rnaseq/qc1"
 
 # trim adapters using cutadapt
 cat data/rnaseq/runids.txt | parallel "cutadapt -m 20 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTC \
@@ -28,24 +29,28 @@ cat data/rnaseq/runids.txt | parallel "cutadapt -m 20 -a AGATCGGAAGAGCACACGTCTGA
 
 # filter reads mapping to rRNA and tRNA genes
 # build bowtie2 index
-bowtie2-build data/rnaseq/refs/Binfantis_ATCC15697_rRNA_tRNA.fasta data/rnaseq/refs/Binfantis_ATCC15697_rRNA_tRNA
+bowtie2-build data/rnaseq/refs/Binfantis_ATCC15697_rRNA_tRNA.fasta \
+data/rnaseq/refs/Binfantis_ATCC15697_rRNA_tRNA
 # align reads via bowtie2; save ones that did not align to a separate file
-cat data/rnaseq/runids.txt | parallel "bowtie2 -x data/rnaseq/refs/Binfantis_ATCC15697_rRNA_tRNA \
+cat data/rnaseq/runids.txt | \
+parallel "bowtie2 -x data/rnaseq/refs/Binfantis_ATCC15697_rRNA_tRNA \
 -U data/rnaseq/fq_trim/{}.fastq.gz \
 -S data/rnaseq/sam/{}.sam \
 --un data/rnaseq/fq_filt/{}.fastq \
 &> data/rnaseq/fq_filt/{}.log"
-rm -rf data/rnaseq/sam
 cat data/rnaseq/runids.txt | parallel "gzip data/rnaseq/fq_filt/{}.fastq"
 
 # run fastqc on filtered reads
-cat data/rnaseq/runids.txt | parallel "fastqc data/rnaseq/fq_filt/{}.fastq.gz --outdir data/rnaseq/qc2"
+cat data/rnaseq/runids.txt | parallel "fastqc data/rnaseq/fq_filt/{}.fastq.gz \
+--outdir data/rnaseq/qc2"
 
 # pseudolalign reads to transcriptome
 # build kallisto index
-kallisto index -i data/rnaseq/refs/Binfantis_ATCC15697_transcriptome.index data/rnaseq/refs/Binfantis_ATCC15697_transcriptome.fasta
+kallisto index -i data/rnaseq/refs/Binfantis_ATCC15697_transcriptome.index \
+data/rnaseq/refs/Binfantis_ATCC15697_transcriptome.fasta
 # map reads to indexed reference via kallisto
-cat data/rnaseq/runids.txt | parallel "kallisto quant -i data/rnaseq/refs/Binfantis_ATCC15697_transcriptome.index \
+cat data/rnaseq/runids.txt | parallel "kallisto quant \
+-i data/rnaseq/refs/Binfantis_ATCC15697_transcriptome.index \
 -o data/rnaseq/kallisto/{} \
 --single \
 -l 200 \
@@ -57,3 +62,9 @@ data/rnaseq/fq_filt/{}.fastq.gz \
 export LC_ALL=en_US.utf-8
 export LANG=en_US.utf-8
 multiqc -d . -o data/rnaseq
+
+# remove directories with intermediate files
+rm -rf data/rnaseq/qc1
+rm -rf data/rnaseq/qc2
+rm -rf data/rnaseq/fq_filt
+rm -rf data/rnaseq/sam
